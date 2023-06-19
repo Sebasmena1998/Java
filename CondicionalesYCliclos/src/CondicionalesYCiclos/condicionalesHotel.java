68b5db609f70bfb05d711fda2aad79a6b6de68e2
Sebasmena98 <smendozan98@gmail.com>
2023-06-18 23:40:22 -0600

    Actualización del los ejercicios de Java

--- CondicionalesYCliclos/src/CondicionalesYCiclos/condicionalesHotel.java
+++ CondicionalesYCliclos/src/CondicionalesYCiclos/condicionalesHotel.java
@@ -7,49 +7,129 @@ public class condicionalesHotel {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	
-	//1er Requerimento: Servicios Adicionales
-	System.out.println("Servicios adicionales");
-	
-	System.out.println("Usted solicito Servicios adicionales? 1.- Si escriba true 2.- No, escriba false"); //Se pregunta si hubo usos de servicios adicionales
-	
-	Scanner solicitud = new Scanner(System.in);
-		
-		
-		boolean serviciosAdicionales = solicitud.nextBoolean();
-		
-		if(serviciosAdicionales) { //Si hay servicios se le hara el cargo de lo que ocupo a su cuenta
-			System.out.println("Usted solicito Servicios Adicionales, se le hara un cargo extra");
-			
-			
-		}else { //Como no ocupo 
-			System.out.println("Usted NO solicito Servicios Adicionales");
+		Scanner scanner = new Scanner(System.in);
+
+		// Servicios Adicionales
+		System.out.println("<-- Servicios Adicionales -->");
+
+		System.out.println("Total de servicios:");
+		double totalServicio = scanner.nextDouble();
+		System.out.println("Total de servicios adicionales:");
+		double totalAdicional = scanner.nextDouble();
+
+		if(totalServicio > 0){
+
+			double precioFinal = totalServicio;
+
+			if (totalAdicional > 0) {
+				System.out.println("Usted adquirio servicios adicionales.\n"
+						+ "Usted tendra un cargo adicional de $" + totalAdicional);
+				precioFinal += totalAdicional;
+			}
+			else if (totalAdicional == 0){
+				System.out.println("Sin servicios adicionales.");
+			}
+			else {
+				System.out.println("Error en los los servicios adicionales.");
+				totalAdicional = 0;
+			}
+
+			System.out.println("Precio del servicio:          $" + totalServicio);
+			System.out.println("Precio de servicio adicional: $" + totalAdicional);
+			System.out.println("Precio final:                 $" + precioFinal);
 
 		}
-	
-	
-	
+		else {
+			System.out.println("Error, no hay precio valido del servicio.");
+		}
 
-	//2do Requerimento Temporada
-	System.out.println();
-	
-	
 
-	//3ro Requerimento	Horario (Check-in y Check-out)
-	System.out.println();
-	
+		// Temporada (dinamica)
+		
+		System.out.println("<-- Temporada (Dinamica) -->");
 
-	//4to Requerimento	
-	System.out.println();
-	
+		System.out.println("Ingrese la temporada:\n"
+				+ "1.- Primavera\n"
+				+ "2.- Verano\n"
+				+ "3.- Otoño\n"
+				+ "4.- Invierno\n");
 
-	//5to Requerimento	
-	System.out.println();
-	
-	
-	
-	
-	solicitud.close();
+		int valorTemporada = scanner.nextInt();
+		
+		double descuento;
+
+		switch (valorTemporada) {
+			case 1 -> {
+				descuento = 12;
+			}
+			case 2 -> {
+				descuento = 7;
+			}
+			case 3 -> {
+				descuento = 15;
+			}
+			case 4 -> {
+				descuento = 5;
+			}
+			default -> descuento = 0;
+		}
+		
+		System.out.println("El descuento es de: " + descuento + "%");
+		
+		// <-- Check-out Check-in -->
+		System.out.println("<-- CheckIn - CheckOut -->");
+
+		// Obtengo la hora del CheckIn
+		System.out.println("Ingrese el horario del Check-In");
+		String checkIn = scanner.next();
+		// Obtengo la hora del CheckOut
+		System.out.println("Ingrese el horario del Check-Out");
+		String checkOut = scanner.next();
+		
+		System.out.println("El horario del CheckIn: " + checkIn);
+		System.out.println("El horario del CheckOut: " + checkOut);
+
+		// Roomservice
+		System.out.println("<-- Room Service -->");
+
+		System.out.println("""
+				¿Quiere servicio a la habitacion?
+				1.- Si
+				2.- No""");
+
+		int opcion = scanner.nextInt();
+
+		if(opcion == 1){
+			System.out.println("Enviando servicio la habitacion");
+		}
+		else if(opcion == 2){
+			System.out.println("Sin cambios");
+		}
+		else {
+			System.out.println("Opcion invalida");
+		}
+
+		// Huespedes extra
+		System.out.println("<-- Huespedes extra -->");
+
+		double precioPorHuespedAdicional = 123.1;
+
+		System.out.println("Numero de huepedes:");
+		int numeroHuepedes = scanner.nextInt();
+		System.out.println("Numero de huepedes reales:");
+		int numeroHuespedesReales = scanner.nextInt();
+
+		if(numeroHuepedes < numeroHuespedesReales){
+			double precioAdicional = (numeroHuespedesReales - numeroHuepedes) * precioPorHuespedAdicional;
+			System.out.println("Sobrepaso el numero de huespedes.");
+			System.out.println("Costo adicional de: $" + precioAdicional);
+		}
+		else {
+			System.out.println("Reservacion sin comflictos.");
+		}
+
+		// cerrando el scanner
+		scanner.close();
+		
 	}
-
 }
